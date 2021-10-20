0d2ccf230f374a46eec6d9c87b70524b3bfc09fb
mosorio <mercedes.osorio.o@gmail.com>
2021-10-20 18:19:19 -0300

    clase configuration CORS

--- src/main/java/com/taller/concesionario/ConcesionarioApplication.java
+++ src/main/java/com/taller/concesionario/ConcesionarioApplication.java
@@ -19,7 +19,7 @@ public class ConcesionarioApplication {
 	// 	return new WebMvcConfigurer() {
 	// 		@Override
 	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 			registry.addMapping("/**").allowedOrigins("http://localhost:21450");
+	// 			registry.addMapping("/**").allowedOrigins("http://localhost:8080");
 	// 		}
 	// 	};
 	// }
